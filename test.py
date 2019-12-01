050552c4b30f258a0e9e18b6737a09ca26708574
florenceinstone <florence.instone@gmail.com>
2019-12-01 14:12:15 +0000

    edit

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print "Hello world"
+def add_two_numbers():
+    number1 = 5
+    number2 = 7
+    answer = number1 + number2
+    return answer
+returned_value = add_two_numbers()
+print returned_value
